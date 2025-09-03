dc6a0a26b1892dc02a9108266e9a5e6d28860892
Radhajanagouda <42607061+Radhajanagouda@users.noreply.github.com>
2025-09-03 20:18:26 +0530

    Correction in threadlocal

--- src/main/java/DriverManager.java
+++ src/main/java/DriverManager.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
-    public static ThreadLocal<WebDriver> ldriver = new ThreadLocal<>();
+    private static ThreadLocal<WebDriver> ldriver = new ThreadLocal<>();
 
     // below works awesome when synchronous execution of threads occur, not to be used in parallel
     //below is called singleton is file pattern
